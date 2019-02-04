42791a743d5c8f65587bdf0d894fe0127021e3d3
Ragupathi <ragupathi@revmakx.com>
2019-02-04 18:45:43 +0530

    vuejs with expressjs

---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <b-form inline>
+      <label class="sr-only" for="inlineFormInputName2">Name</label>
+      <b-input class="mb-2 mr-sm-2 mb-sm-0" v-model="form.email" id="inlineFormInputName2" placeholder="Jane Doe" />
+      <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
+      <b-input-group left="@" class="mb-2 mr-sm-2 mb-sm-0">
+        <b-input id="inlineFormInputGroupUsername2" v-model="form.password" placeholder="Username" />
+      </b-input-group>
+      <b-button @click="save" variant="primary">Login</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() { 
+    return {
+      form:{} 
+    }
+  },
+  methods:{ 
+    save() {
+      console.log('Hello World')
+    }
+  }
+}
+</script>
